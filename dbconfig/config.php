7ca02442d559f24197a1d45e51da7ab80954af1b
Nagendra babu Shakamuri <nbabu724@gmail.com>
2021-12-31 03:14:59 +0530

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php 
-$con = mysqli_connect('localhost','root','') or die('Unable to connect');
-mysqli_select_db($con,'projectdb');
+$con = mysqli_connect('sql6.freesqldatabase.com','sql6462312','yrKARKdRpy') or die('Unable to connect');
+mysqli_select_db($con,'sql6462312');
 ?>
