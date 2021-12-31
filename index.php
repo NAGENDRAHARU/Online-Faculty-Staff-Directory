8f0973565f9fdb7c365e5126247b4591c79b42b0
Nagendra babu Shakamuri <nbabu724@gmail.com>
2021-12-31 23:52:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -45,29 +45,41 @@ SRM FACULTY STAFF DIRECTORY
   <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
 <p style='color:black'>
   Online faculty directory enables user to effortlessly view college/university faculty details. Student can simply view different college/university faculty details anywhere required at any time as this application is handy. This system is built with effective graphical user interface which enables user openness. User can search faculty details and view their particular details such as name, department, courses, area of expertise, and professional interest. This system reduced time and cost of user. Here there are two entities who will access this system i.e. admin and student. Admin is authorized to add and manage all the faculty details. User can directly search for faculty and view their details. Data in database are maintained securely without any maintenance cost.
 </p>
 </div>
 <script>
-var slideIndex = 0;
-showSlides();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+ 
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+      slides[i].style.display = "none";  
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 </script>
 
