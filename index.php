bbe5af67552479c34064f75b9b5f72c1e9c88cdb
Nagendra babu Shakamuri <nbabu724@gmail.com>
2022-01-01 00:00:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -40,9 +40,6 @@ SRM FACULTY STAFF DIRECTORY
     <div class="numbertext">3 / 3</div>
     <img src="3.jpeg" style="width:100%; height: 100%;">
   </div>
- <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
 <div style="text-align:center">
@@ -55,31 +52,24 @@ SRM FACULTY STAFF DIRECTORY
 </p>
 </div>
 <script>
-var slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
- 
-function showSlides(n) {
+function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 </script>
 
