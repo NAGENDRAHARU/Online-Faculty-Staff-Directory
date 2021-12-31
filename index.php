80c88803a2fcd38651f0bb4cacd19deb9cbfba3e
Nagendra babu Shakamuri <nbabu724@gmail.com>
2021-12-31 06:42:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -29,33 +29,19 @@ SRM FACULTY STAFF DIRECTORY
   <div class="mySlides fade">
     <div class="numbertext">1 / 3</div>
     <img src="1.jpeg" style="width:100%; height: 100%;">
-    <div class="text">Caption Text</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">2 / 3</div>
     <img src="2.jpeg" style="width:100%; height: 100%;">
-    <div class="text">Caption Two</div>
   </div>
 
   <div class="mySlides fade">
     <div class="numbertext">3 / 3</div>
     <img src="3.jpeg" style="width:100%; height: 100%;">
-    <div class="text">Caption Three</div>
   </div>
-
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
 </div>
 <br>
-
-<!-- The dots/circles -->
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-</div>
 <p style='color:black'>
   Online faculty directory enables user to effortlessly view college/university faculty details. Student can simply view different college/university faculty details anywhere required at any time as this application is handy. This system is built with effective graphical user interface which enables user openness. User can search faculty details and view their particular details such as name, department, courses, area of expertise, and professional interest. This system reduced time and cost of user. Here there are two entities who will access this system i.e. admin and student. Admin is authorized to add and manage all the faculty details. User can directly search for faculty and view their details. Data in database are maintained securely without any maintenance cost.
 </p>
