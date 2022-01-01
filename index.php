8d5cf5232f30fb535bbc7323d1ca7334ef6458a9
SN078361 <sn078361@cerner.net>
2022-01-01 19:18:45 +0530

    20.1

--- index.php
+++ index.php
@@ -1,76 +1,29 @@
 <?php
 session_start();
 ?>
-<!doctype html>
-<html>
-<head>
-<title>Home</title>
-<link rel="stylesheet" href="css/homecss.css">
-</head>
-<body>
-<div class="navbar">
- <a href="index.php">Home</a>
- <a href="#contact">Contact</a>
- <div class="topnav-right">
- <a href="signup.php">Signup</a>
- <a href="login.php">Login</a>
- </div>
-</div>
-<div class="title">
-<center>
-<h1>
-SRM FACULTY STAFF DIRECTORY
-</h1>
-</center>
-<div class="slideshow-container">
-
-  <!-- Full-width images with number and caption text -->
-  <div class="mySlides fade">
-    <div class="numbertext">1 / 3</div>
-    <img src="slides/1.jpeg" style="width:100%; height: 100%;">
-  </div>
-
-  <div class="mySlides fade">
-    <div class="numbertext">2 / 3</div>
-    <img src="slides/2.jpeg" style="width:100%; height: 100%;">
-  </div>
-
-  <div class="mySlides fade">
-    <div class="numbertext">3 / 3</div>
-    <img src="slides/3.jpeg" style="width:100%; height: 100%;">
-  </div>
-</div>
-<br>
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span>
-  <span class="dot" onclick="currentSlide(2)"></span>
-  <span class="dot" onclick="currentSlide(3)"></span>
-</div>
-<p style='color:black'>
-  Online faculty directory enables user to effortlessly view college/university faculty details. Student can simply view different college/university faculty details anywhere required at any time as this application is handy. This system is built with effective graphical user interface which enables user openness. User can search faculty details and view their particular details such as name, department, courses, area of expertise, and professional interest. This system reduced time and cost of user. Here there are two entities who will access this system i.e. admin and student. Admin is authorized to add and manage all the faculty details. User can directly search for faculty and view their details. Data in database are maintained securely without any maintenance cost.
-</p>
-</div>
-<script>
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-</script>
-
-</body>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/homecss.css">
+  </head>
+  <body>
+    <div class="banner">
+      <div class="navbar">
+        <img src="img/logo.png" class="logo">
+        <ul>
+          <li><a href="login.php">Login</a></li>
+          <li><a href="signup.php">Signup</a></li>
+        </ul>
+      </div>
+      <div class="content">
+        <h1>FACULTY STAFF DIRECTORY</h1>
+        <p>Find the professor who suits your research interest.<br>View their particular details such as name, department, courses, area of expertise, and professional interest.</p>
+        <div>
+          <button type="button"><span></span>Login</button>
+          <button type="button"><span></span>SignUp</button>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
