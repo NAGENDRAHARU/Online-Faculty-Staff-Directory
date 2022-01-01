5d87cfc32f0b763d5175f8a4705d7d869d633ff6
SN078361 <sn078361@cerner.net>
2022-01-01 14:01:33 +0530

    20.1

--- search.php
+++ search.php
@@ -111,7 +111,7 @@ if(isset($_GET['submit']))
 $name = $_GET['search'];
 $query = "select * from professors where name like '%$search%'";
 $query_run = mysqli_query($con,$query);
-echo mysqli_num_rows($query_run);
+echo '<script type="text/javascript">alert("'.mysqli_num_rows($query_run).'")</script>';
 if(mysqli_num_rows($query_run)>0){
   $_SESSION['search'] = $name;
   header('location:faculty.php');
