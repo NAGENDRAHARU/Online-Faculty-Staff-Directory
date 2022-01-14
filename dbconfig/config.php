2d27b2d8c5eab44331d21f02b243ca4d9c6087dc
Nagendra babu Shakamuri <nbabu724@gmail.com>
2022-01-14 07:35:58 +0530

    Update config.php

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php 
 $con = mysqli_connect('database-1.cytbxke9fgnl.ap-south-1.rds.amazonaws.com','admin','20Aug1999') or die('Unable to connect');
-mysqli_select_db($con,'admin');
+mysqli_select_db($con,'srm');
 ?>
